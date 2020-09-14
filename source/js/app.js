6a88f043a44d103af0a9044048fb42e3ff1ae95a
HerzogKlaus <maxspace2000@mail.ru>
2020-09-14 18:18:56 +0500

    1

--- source/js/app.js
+++ source/js/app.js
@@ -1,12 +1,26 @@
-let btn = document.querySelector(".menu__close");
-
+let btn = document.querySelector(".menu__btn");
+let menuElem = document.querySelectorAll(".menu__link");
 btn.addEventListener("click", (e) => {
   if (e.target.classList.contains("menu__close")) {
     e.target.classList.add("menu__burger-btn");
     e.target.classList.remove("menu__close");
+    menuElem.forEach((elem) => {
+      if (!elem.classList.contains("menu__link--logo")) {
+        elem.classList.add("close");
+      } else {
+        return;
+      }
+    });
   } else {
+    console.log('111');
     e.target.classList.add("menu__close");
     e.target.classList.remove("menu__burger-btn");
+    menuElem.forEach((elem) => {
+      if (!elem.classList.contains("menu__link--logo")) {
+        elem.classList.remove("close");
+      } else {
+        return;
+      }
+    });
   }
 });
-
